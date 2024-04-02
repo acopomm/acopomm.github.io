3fc62943cc871252ddb5895bd4e78689d68e564b
acopomm <acopomm@users.noreply.github.com>
2024-04-03 00:16:54 +0800

    datetime feedback

--- scripts.js
+++ scripts.js
@@ -4,3 +4,17 @@ function tymsg() {
       document.getElementById("thanku").style.opacity = "0"
     }, 1000)
   }
+{
+  var d = new Date();
+}
+
+function rating(r) {
+  return r
+}
+
+function msgchange(a){
+  document.getElementById("rating").innerHTML = a
+  document.getElementById("month").innerHTML = d.getMonth
+  document.getElementById("day").innerHTML = d.getDate
+  document.getElementById("time").innerHTML = d.getHours
+}
