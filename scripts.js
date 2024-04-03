b925026dc85a16b96787aca1c387b0bc432cfeac
acopomm <acopomm@users.noreply.github.com>
2024-04-03 13:55:16 +0800

    ahaha

--- scripts.js
+++ scripts.js
@@ -25,3 +25,18 @@ function msgchange(a){
   document.getElementById("time").innerHTML = time
 }
 
+window.addEventListener("load", function() {
+  const form = document.getElementById('dataform');
+  form.addEventListener("submit", function(e) {
+    e.preventDefault();
+    const data = new FormData(form);
+    const action = e.target.action;
+    fetch(action, {
+      method: 'POST',
+      body: data,
+    })
+    .then(() => {
+      alert("Success!");
+    })
+  });
+});
