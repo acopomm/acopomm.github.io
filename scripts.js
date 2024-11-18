702e93e962964fa58b8e1b5e1c36edf0d8c70508
lib <acopomm@gmail.com>
2024-11-18 17:15:50 +0800

    ok

--- scripts.js
+++ scripts.js
@@ -3,7 +3,7 @@ function tymsg() {
   document.body.style.backgroundImage = 'linear-gradient(rgba(145, 250, 162,0) 85%, rgba(145, 250, 162,1))';
   setTimeout(() => {
     document.getElementById("thanku").style.opacity = "0";
-    document.body.style.backgroundColor= "rgb(242, 244, 235)";
+    document.body.style.backgroundImage = 'linear-gradient(rgba(145, 250, 162,0) 85%, rgba(145, 250, 162,0))';
   }, 1000)
 }
 
