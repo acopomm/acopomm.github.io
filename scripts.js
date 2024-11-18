5c4286a625c2444aee45dae471577914a433d795
lib <acopomm@gmail.com>
2024-11-18 16:22:53 +0800

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -7,33 +7,33 @@ function tymsg() {
     }, 1000)
   }
 
-function rate(a) {
-  var d = new Date();
-  var day = d.getDate();
-  var month = d.getMonth();
-  var year = d.getFullYear();
-  var time = d.getHours();
-  document.getElementById("dataform").reset();
-  document.getElementById("FormDate").value= month+"/"+day+"/"+year;
-  document.getElementById("FormTime").value= time+":00";
-  document.getElementById("Rating").value=a;
-  document.getElementById('submitbutton').click();
-  tymsg()
-}
-
-
-window.addEventListener("load", function() {
-  const form = document.getElementById('dataform');
-  form.addEventListener("submit", function(e) {
-    e.preventDefault();
-    const data = new FormData(form);
-    const action = e.target.action;
-    fetch(action, {
-      method: 'POST',
-      body: data,
-    })
-    .then(() => {
-      ;
-    })
+  function rate(a) {
+    var d = new Date();
+    var day = d.getDate();
+    var month = d.getMonth()+1;
+    var year = d.getFullYear();
+    var time = d.getHours();
+    document.getElementById("dataform").reset();
+    document.getElementById("FormDate").value= month+"/"+day+"/"+year;
+    document.getElementById("FormTime").value= time+":00";
+    document.getElementById("Rating").value=a;
+    document.getElementById('submitbutton').click()
+    tymsg();
+  }
+  
+  window.addEventListener("load", function() {
+    const form = document.getElementById('dataform');
+    form.addEventListener("submit", function(e) {
+      e.preventDefault();
+      const data = new FormData(form);
+      const action = e.target.action;
+      fetch(action, {
+        method: 'POST',
+        body: data,
+      })
+      .then(() => {
+        tymsg();
+      })
+    });
   });
-});
+  
