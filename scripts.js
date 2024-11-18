4d4c6ffbb1f1bdf4029cdb4bdabf0bebf2d5c0db
lib <acopomm@gmail.com>
2024-11-18 16:25:41 +0800

    Push

--- scripts.js
+++ scripts.js
@@ -1,39 +1,36 @@
 function tymsg() { 
-    document.getElementById("thanku").style.opacity = "100";
-    document.body.style.background = linear-gradient(#f2f4eb, #adeb9d);
-    setTimeout(() => {
-      document.getElementById("thanku").style.opacity = "0";
-      document.body.style.background = "#f2f4eb"
-    }, 1000)
-  }
+  document.getElementById("thanku").style.opacity = "100";
+  setTimeout(() => {
+    document.getElementById("thanku").style.opacity = "0"
+  }, 1000)
+}
 
-  function rate(a) {
-    var d = new Date();
-    var day = d.getDate();
-    var month = d.getMonth()+1;
-    var year = d.getFullYear();
-    var time = d.getHours();
-    document.getElementById("dataform").reset();
-    document.getElementById("FormDate").value= month+"/"+day+"/"+year;
-    document.getElementById("FormTime").value= time+":00";
-    document.getElementById("Rating").value=a;
-    document.getElementById('submitbutton').click()
+function rate(a) {
+var d = new Date();
+var day = d.getDate();
+var month = d.getMonth()+1;
+var year = d.getFullYear();
+var time = d.getHours();
+document.getElementById("dataform").reset();
+document.getElementById("FormDate").value= month+"/"+day+"/"+year;
+document.getElementById("FormTime").value= time+":00";
+document.getElementById("Rating").value=a;
+document.getElementById('submitbutton').click()
+tymsg();
+}
+
+window.addEventListener("load", function() {
+const form = document.getElementById('dataform');
+form.addEventListener("submit", function(e) {
+  e.preventDefault();
+  const data = new FormData(form);
+  const action = e.target.action;
+  fetch(action, {
+    method: 'POST',
+    body: data,
+  })
+  .then(() => {
     tymsg();
-  }
-  
-  window.addEventListener("load", function() {
-    const form = document.getElementById('dataform');
-    form.addEventListener("submit", function(e) {
-      e.preventDefault();
-      const data = new FormData(form);
-      const action = e.target.action;
-      fetch(action, {
-        method: 'POST',
-        body: data,
-      })
-      .then(() => {
-        tymsg();
-      })
-    });
-  });
-  
+  })
+});
+});
